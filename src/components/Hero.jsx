8a7bf267cd2e5adbc6433d9401c088520b79c438
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-09-03 15:26:47 +0300

    video carousel end

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,9 +35,7 @@ const Hero = () => {
 	return (
 		<section className='w-full nav-height bg-black relative'>
 			<div className='h-5/6 w-full flex-center flex-col'>
-				<p
-					id='hero'
-					className='hero-title'>
+				<p id='hero' className='hero-title'>
 					iPhone 15 Pro
 				</p>
 				<div className='md:w-10/12 w-9/12'>
@@ -47,19 +45,14 @@ const Hero = () => {
 						playsInline={true}
 						key={videoSrc}
 						className='pointer-events-none'>
-						<source
-							src={videoSrc}
-							type='video/mp4'
-						/>
+						<source src={videoSrc} type='video/mp4' />
 					</video>
 				</div>
 			</div>
 			<div
 				id='cta'
 				className='flex flex-col items-center opacity-0 translate-y-20'>
-				<a
-					href='#highlights'
-					className='btn'>
+				<a href='#highlights' className='btn'>
 					Buy
 				</a>
 				<p className='font-normal text-xl'>From $199/month or $999</p>
